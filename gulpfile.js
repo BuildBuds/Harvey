b18a6dbd1159dc9c6e8cccdf2d98e5bc066f5f35
Aaron Baker <aaron.baker@ibm.com>
2017-09-05 20:27:17 -0500

    mid gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,59 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  var scss = gulp.src('./src/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer())
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass']);
+gulp.task('sass:watch', ['sass'], function() {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+})
+
+
+// JavaScript
+gulp.task('js', function() {
+  gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js:watch', ['js'], function() {
+  gulp.watch('src/js/**/*.js', ['js']);
+});
+
+
+// BrowserSync
+gulp.task('serve', ['build'], () => {
+  browserSync.init({
+    server: {
+      // TODO: make this a dynamic environment variable
+      proxy: 'localhost:3000'
+    }
+  });
+});
+
+// Watch
+gulp.task('watch', ['sass:watch', 'js:watch']);
+
+// Build
+gulp.task('build', ['sass', 'js']);
+
+//////////////////////////////
+// Default
+//////////////////////////////
+gulp.task('default', ['serve', 'watch']);
